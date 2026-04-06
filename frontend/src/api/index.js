597843c3f344292dff46fe5e14950ccea62d9e68
Joseph Conduah <josephconduah02@gmail.com>
2026-04-06 01:25:22 +0000

    finally

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 // src/api/index.js
 import axios from 'axios'
 
-const BASE_URL = 'https://terranova-backend-8kus.onrender.com/api'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
 
 const api = axios.create({ baseURL: BASE_URL })
 
